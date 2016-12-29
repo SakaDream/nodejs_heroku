96f41d79eee178c842bb2b0406f54d41afa89380
SakaDream <bahaimt@gmail.com>
2016-12-29 20:39:14 +0700

    fix Digest already called

--- index.js
+++ index.js
@@ -220,9 +220,6 @@ app.post("/login", urlencodedParser, function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
 
-    var crypt = md5.update(password);
-    var passCrypt = crypt.digest('hex');
-
     pool.connect(function (err, client, done) {
         if (err) {
             return console.error('error fetching client from pool', err);

1094f91d9617e6cb4a6785b6d264bf395aed0358
SakaDream <bahaimt@gmail.com>
2016-12-29 22:16:55 +0700

    fix error but continue insert user record to db #2

--- index.js
+++ index.js
@@ -279,7 +279,7 @@ app.post("/register", urlencodedParser, function (req, res) {
                 for (var i = 0; i < result.rows.length; i++) {
                     if (result.rows[i].USERNAME.toString().trim() === username.toString().trim()) {
                         error = 'Username đã trùng';
-                        return res.render("login", { hiddenLG: hiddenLG, hiddenSU: hiddenSU, error: error });
+                        res.render("login", { hiddenLG: hiddenLG, hiddenSU: hiddenSU, error: error });
                         flag = 1;
                     }
                 }

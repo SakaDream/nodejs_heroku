07e9b2a2b8d7500dd36ee35f7455d26e8494c18e
SakaDream <bahaimt@gmail.com>
2017-01-23 11:00:11 +0700

    fix disabled class

--- index.js
+++ index.js
@@ -67,10 +67,6 @@ app.get("/videos/list", function (req, res) {
         properties.disabled = "";
         properties.name = "edit";
         properties.icon = "fa fa-edit";
-    } else if(req.session.roleid == undefined) {
-        res.send("roleid is undefined!");
-    } else {
-        res.send("Output: " + req.session.roleid);
     }
     pool.connect(function (err, client, done) {
         if (err) {

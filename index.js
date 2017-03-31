a46162bb2f8c9fd521025d5e73c409639b33dee6
root <root@ubuntu-512mb-sgp1-01.localdomain>
2017-03-31 04:51:56 +0000

    Set SSL

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ var config = {
     port: 5432, //env var: PGPORT
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    ssl: true // set ssl connection
 };
 var pool = new pg.Pool(config);
 
